dcaa6ba918fdf813453061210d00db0fecd06349
bsorrentino <bartolomeo.sorrentino@gmail.com>
2024-03-26 13:01:38 +0100

    build: add primefaces sample

---
+++ primefaces-chat-app/src/main/java/com/chatapp/PrimefacesChatAppApplication.java
@@ -0,0 +1,11 @@
+package com.chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimefacesChatAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PrimefacesChatAppApplication.class, args);
+    }
+}
